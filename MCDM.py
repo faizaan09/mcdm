0f06dee81ba9d6b3478fae9f77f03258481d49c6
faizaan09 <faizaancharania@gmail.com>
2016-03-27 09:29:49 +0530

    Generalized input from file with structured input display

--- MCDM.py
+++ MCDM.py
@@ -1,34 +1,42 @@
-
-
-print "Enter the number of alternatives"
+#print "Enter the number of alternatives"
 alternativeCount = int(raw_input())
-print "Enter the number of attributes"
+#print "Enter the number of attributes"
 attributeCount = int(raw_input())
 
 Alternatives = [["" for x in range(attributeCount)] for x in range(alternativeCount)]
 
 weights = []
 for x in xrange(len(Alternatives[0])):
-	print "Enter the weights for the attributes"
+	#print "Enter the weights for the attributes"
 	weights.append(float(raw_input()))
 
 for i in range(len(Alternatives)):
-	print"Enter attribute values for alternative ",i + 1,":"
+	#print"Enter attribute values for alternative ",i + 1,":"
 	for j in range(len(Alternatives[i])):
-		print"Attribute ",j,":"
+		#print"Attribute ",j,":"
 		Alternatives[i][j] = int(raw_input())
+
+print "Number of alternatives:", alternativeCount
+print "Number of attributes:", attributeCount
+print "Weights of every attribute are: "
+for i in range(len(weights)):
+	print weights[i]
+
+print "All the alternatives with attribute specific scores are: "
+for i in range(alternativeCount):
+	print Alternatives[i]
 
 scores = [0.0 for i in range(alternativeCount)]
 for i in range(3):
 	for j in range(4):
 		scores[i] += Alternatives[i][j]**weights[j]
-print "\n\n\n"
+print "\n\n"
 print "Acording to WPM, the scores of the alternatives are: "
 for i in range(3):
 	print scores[i]
 
-print "The best WPM alternative is alternative", scores.index(max(scores)) + 1
-print "\n\n\n"
+print "\nThe best WPM alternative is alternative", scores.index(max(scores)) + 1
+print "\n\n"
 
 scores = [0.0 for i in range(3)]
 for i in range(3):
@@ -39,4 +47,4 @@ print "Acording to SAW, the scores of the alternatives are: "
 for i in range(3):
 	print scores[i]
 
-print "The best SAW alternative is alternative", scores.index(max(scores)) + 1
+print "\nThe best SAW alternative is alternative", scores.index(max(scores)) + 1
